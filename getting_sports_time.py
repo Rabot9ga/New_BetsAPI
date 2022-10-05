da3c6249101f422872f924af02490b98ba99102b
Rabot9ga <freedomboxnikita@inbox.ru>
2022-10-05 18:36:11 +0300

    'class_realized'

--- getting_sports_time.py
+++ getting_sports_time.py
@@ -4,19 +4,43 @@ import re
 from datetime import datetime as DT, timedelta
 
 
-def sport_time(pages_number: int, header: dict, sport: str) -> None:
-    counter_dict={}
-    for i in range(1, pages_number + 1):
-        req = requests.get('https://betsapi.com/cs/'+sport+'/p.' + str(i) + '?skipE=1', headers=header)
+class Sports:
+
+    def __init__(self, sport: str, cookie: str) -> None:
+        self.__sport = sport
+        self.__cookie = cookie
+
+    def header(self):
+        return {
+                    'cookie': self.__cookie,
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+                    'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+                    'upgrade-insecure-requests': '1'
+                }
+
+    def __pages_counting(self) -> int:
+
+        req = requests.get('https://betsapi.com/cs/' + self.__sport + '/p.1?skipE=1', headers=self.header())
         soup = BeautifulSoup(req.text, 'html.parser')
-        lst = re.findall(r"\d\d:\d\d", soup.get_text())
+        lst = re.findall(r"\/cs\/" + self.__sport + "\/p.\d+", soup.prettify())
+        new_lst = []
         for i in lst:
-            dt_fmt = '%H:%M'
-            time = (DT.strptime(i, dt_fmt) + timedelta(hours=3)).strftime(dt_fmt)
-            if time in counter_dict:
-                counter_dict[time] += 1
-            else:
-                counter_dict[time] = 1
-    with open(sport+'.txt', 'w') as file:
-        for i in counter_dict:
-            file.write(str(i) + ',' + str(counter_dict[i]) + '\n')
+            new_lst.append(re.search(r"\d+", i)[0])
+        return max(list(map(int, new_lst)))
+
+    def sport_time(self) -> None:
+        counter_dict={}
+        for i in range(1, self.__pages_counting() + 1):
+            req = requests.get('https://betsapi.com/cs/'+self.__sport+'/p.' + str(i) + '?skipE=1', headers=self.header())
+            soup = BeautifulSoup(req.text, 'html.parser')
+            lst = re.findall(r"\d\d:\d\d", soup.get_text())
+            for i in lst:
+                dt_fmt = '%H:%M'
+                time = (DT.strptime(i, dt_fmt) + timedelta(hours=3)).strftime(dt_fmt)
+                if time in counter_dict:
+                    counter_dict[time] += 1
+                else:
+                    counter_dict[time] = 1
+        with open(self.__sport+'.txt', 'w') as file:
+            for i in counter_dict:
+                file.write(str(i) + ',' + str(counter_dict[i]) + '\n')
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import counting_pages
-import getting_sports_time
+from getting_sports_time import Sports
 import tkinter, tkinter.messagebox
 
 root = tkinter.Tk()
@@ -20,17 +19,11 @@ cookieField.place(x=120, y=12)
 
 def main():
     cookies = cookieField.get()
-    header = {
-        'cookie': cookies,
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-        'upgrade-insecure-requests': '1'
-    }
-    pages_soccer_number: int = counting_pages.pages_counting(header, 'soccer')
-    pages_tennis_number: int = counting_pages.pages_counting(header, 'tennis')
-    getting_sports_time.sport_time(pages_soccer_number, header, 'soccer')
-    getting_sports_time.sport_time(pages_tennis_number, header, 'tennis')
-    tkinter.messagebox.showinfo(title='Success', message='Time for tennis and soccer is created')
+    for i in ['soccer', 'tennis', 'basketball']:
+        sport = Sports(i, cookies)
+        sport.sport_time()
+        
+    tkinter.messagebox.showinfo(title='Success', message='Time for tennis, basketball and soccer is created')
 
 
 btn=tkinter.Button(frame, text='Create results', width=30, command=main)
